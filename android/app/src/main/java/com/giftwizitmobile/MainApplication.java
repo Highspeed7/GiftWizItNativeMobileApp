e6372db0785c009b95d01323acb14984287083a1
Brian West <b.sanders7777@edmail.edcc.edu>
2020-02-09 16:19:36 -0800

    Attempted share extension

--- android/app/src/main/java/com/giftwizitmobile/MainApplication.java
+++ android/app/src/main/java/com/giftwizitmobile/MainApplication.java
@@ -10,8 +10,12 @@ import com.facebook.react.ReactApplication;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.alinz.parkerdan.shareextension.SharePackage;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -24,11 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      return packages;
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new SharePackage()
+      );
     }
 
     @Override
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Share from './Share';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () =>  require('./App').default);
+AppRegistry.registerComponent('GiftWizItShare', () => require('./Share').default);
